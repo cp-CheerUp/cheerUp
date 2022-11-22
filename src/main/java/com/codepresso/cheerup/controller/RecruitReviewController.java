f768ac72afb08a1cd831488f97b67e6a9dbcd56a
yjkim9726 <yjkim9726@gmail.com>
2022-11-22 14:12:51 +0900

    채용 후기 게시판 초기

--- src/main/java/com/codepresso/cheerup/controller/RecruitReviewController.java
+++ src/main/java/com/codepresso/cheerup/controller/RecruitReviewController.java
@@ -1,4 +1,15 @@
 package com.codepresso.cheerup.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path="recruitReview")
 public class RecruitReviewController {
+
+    @GetMapping("/mainRecruitReview")
+    public String test2(){
+        return "recruitReview/mainRecruitReview";
+    }
 }
