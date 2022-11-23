5c9143fb34d5b74ef150ebe9dbe9b20a94af555a
Jeonminji <minjia13@gmail.com>
2022-11-23 11:04:51 +0900

    Feat: 자유게시판 Controller 글 보기, 작성 추가

--- src/main/java/com/codepresso/cheerup/controller/FreeBoardController.java
+++ src/main/java/com/codepresso/cheerup/controller/FreeBoardController.java
@@ -2,11 +2,23 @@ package com.codepresso.cheerup.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(path="freeboard")
 public class FreeBoardController {
-    @GetMapping("/freeboard")
+    @GetMapping("/main")
     public String freeboard() {
         return "freeBoard/freeboard";
+    }
+
+    @GetMapping("/write")
+    public String freeboardWrite() {
+        return "freeBoard/freeboardWrite";
+    }
+
+    @GetMapping("/view")
+    public String freeboardView() {
+        return "freeBoard/freeboardView";
     }
 }
