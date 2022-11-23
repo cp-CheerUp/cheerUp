e5c8d411f18f66ea0711ad98e3234626f03144d6
jelly02 <haribo__@naver.com>
2022-11-23 14:45:13 +0900

    Feat : User 로그인, 회원가입 back 작업

---
+++ src/main/java/com/codepresso/cheerup/service/UserService.java
@@ -0,0 +1,34 @@
+package com.codepresso.cheerup.service;
+
+import com.codepresso.cheerup.dao.UserMapper;
+import com.codepresso.cheerup.vo.Member;
+import com.manage.foruser.Mapper.UserMapper;
+import com.manage.foruser.vo.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+    private UserMapper userMapper;
+
+    public UserService(UserMapper _userMapper){
+        this.userMapper = _userMapper;
+    }
+
+    //회원가입 처리
+    @Transactional
+    public int insertMember(Member member) {
+        member.setRole("USER");
+        int result = this.userMapper.insertMember(member);
+
+        return result;
+    }
+
+    //로그인 처리
+    public String loginCheck(String id, String password) {
+        String role = userMapper.loginCheck(id,password);
+
+        return role;
+    }
+}
--- src/main/java/com/codepresso/cheerup/vo/Member.java
+++ src/main/java/com/codepresso/cheerup/vo/Member.java
@@ -1,4 +1,20 @@
 package com.codepresso.cheerup.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
+
+    private String id;
+    private String password;
+    private String name;
+    private String phone;
+    private String role;
+    private String education;
+    private String isMajor;
+
 }
