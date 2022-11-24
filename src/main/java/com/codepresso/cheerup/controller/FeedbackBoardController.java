dba3a5bda59f3aee952989a9699339b5f49b859c
Jeonminji <minjia13@gmail.com>
2022-11-24 13:30:47 +0900

    Feat: 피드백 게시판 글 보기, 작성 기능 추가

--- src/main/java/com/codepresso/cheerup/controller/FeedbackBoardController.java
+++ src/main/java/com/codepresso/cheerup/controller/FeedbackBoardController.java
@@ -2,11 +2,23 @@ package com.codepresso.cheerup.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(path="feedback")
 public class FeedbackBoardController {
-    @GetMapping("/feedback")
-    public String feedbackBoard() {
+    @GetMapping("/main")
+    public String feedbackboard() {
         return "feedbackBoard/feedback";
+    }
+
+    @GetMapping("/write")
+    public String feedbackboardWrite() {
+        return "feedbackBoard/feedbackWrite";
+    }
+
+    @GetMapping("/view")
+    public String feedbackboardView() {
+        return "feedbackBoard/feedbackView";
     }
 }
