f408cb515d3eb0472372f0078dea5c0487039238
jelly02 <haribo__@naver.com>
2022-11-29 04:22:04 +0900

    Feat : interview front 처리 끝

--- src/main/java/com/codepresso/cheerup/controller/InterviewController.java
+++ src/main/java/com/codepresso/cheerup/controller/InterviewController.java
@@ -23,7 +23,7 @@ public class InterviewController {
     @PostMapping("/interview")
     public List<Interview> interview_select(Interview interview){
 
-//        Authentication authentication
+//         Authentication authentication
 //        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 //        String id = userDetails.getUsername();
         String category_origin = interview.getCategory();
@@ -45,7 +45,7 @@ public class InterviewController {
             String[] multiArray = category_origin.split(",");
             System.out.println("여기는 컨트롤러고 조건문 복합 카테고리 안에 있어요"+multiArray);
 
-//            //쿼리 갯수 제한 카운트
+            //쿼리 갯수 제한 카운트
             int limitCount = 0;
 
             if(multiArray.length == 2){
@@ -70,14 +70,14 @@ public class InterviewController {
                 limitCount = 9;
             }
 
-            System.out.println(limitCount);
-
 
             List<Interview> multiList = interviewService.getMultiList(multiArray);
-          //  List<Interview> multiListTest = interviewService.getMultiListTest(multiArray,limitCount);
+
+
 
 
             //질문들의 output 카운트 +1 증가
+
             return multiList;
 
         }
--- src/main/java/com/codepresso/cheerup/dao/InterviewMapper.java
+++ src/main/java/com/codepresso/cheerup/dao/InterviewMapper.java
@@ -2,17 +2,34 @@ package com.codepresso.cheerup.dao;
 
 import com.codepresso.cheerup.vo.Interview;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface InterviewMapper {
-    
+
     //유저가 선택한 카테고리의 정보만 가져오기
     List<Interview> getSelectedList(Interview interview);
 
     List<Interview> getSingleList(String category_origin);
     List<Interview> getMultiList(String[] multiArray);
 
-    List<Interview> getMultiListTest(String[] multiArray, int limitCount);
+    List<Interview> getProgrammingList(String category, int limitCount);
+
+    List<Interview> getData_structureList(String category, int limitCount);
+
+    List<Interview> getAlgorithmList(String category, int limitCount);
+
+    List<Interview> getDatabaseList(String category, int limitCount);
+
+    List<Interview> getNetworkList(String category, int limitCount);
+
+    List<Interview> getOsList(String category, int limitCount);
+
+    List<Interview> getPersonalityList(String category, int limitCount);
+
+    List<Interview> getSurpriseList(String category, int limitCount);
+
+    int insertUserResponse(String id, @Param("id_array") String[] id_array, @Param("answer_array") String[] answer_array);
 }
