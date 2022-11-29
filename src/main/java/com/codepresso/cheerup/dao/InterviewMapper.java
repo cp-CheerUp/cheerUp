5cc6af44d7f4db148ac0fff99708f295d34b71db
JuriKim <40787487+jelly02@users.noreply.github.com>
2022-11-29 23:22:36 +0900

    Feat : 면접 시뮬레이션 기능 구현 완료

--- src/main/java/com/codepresso/cheerup/dao/InterviewMapper.java
+++ src/main/java/com/codepresso/cheerup/dao/InterviewMapper.java
@@ -1,6 +1,7 @@
 package com.codepresso.cheerup.dao;
 
 import com.codepresso.cheerup.vo.Interview;
+import com.codepresso.cheerup.vo.Interview_response;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -32,4 +33,36 @@ public interface InterviewMapper {
     List<Interview> getSurpriseList(String category, int limitCount);
 
     int insertUserResponse(String id, @Param("id_array") String[] id_array, @Param("answer_array") String[] answer_array);
+
+    int insertUserResponse2(String id, String interview_id, String interview_answer);
+
+    void updateTotalCount(String id_array);
+
+    List<Interview> getResponseList(String[] id_array);
+
+//    int getRowCount();
+
+    void insertFailCount( @Param("failId") String[] failId );
+
+    void updateFailCounting(@Param("id_list") List<Object> id_list);
+
+    void updateFailCounting2(String failId);
+
+    int getPgCount(@Param("id")String id);
+
+    int getDsCount(@Param("id") String id);
+
+    int getAgCount(@Param("id") String id);
+
+    int getDbCount(@Param("id") String id);
+
+    int getNwCount(@Param("id") String id);
+
+    int getOsCount(@Param("id") String id);
+
+    int getPsCount(@Param("id") String id);
+
+    int getSpCount(@Param("id")  String id);
+
+    List<Interview> getFailList(String id);
 }
